119e5aae9397fe39e703b49b46963ff2d008d68e
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-02-26 23:29:21 +0100

    Changing names I guess

--- brainstorm.rs
+++ brainstorm.rs
@@ -1,36 +1,36 @@
 
 // ----------------------------------
 
-#[derive(glace::Define)]
-struct Define {
-    radians_per_sec: f32,
-}
-
-#[derive(glace::Uniform)]
+#[derive(Uniform)]
 struct ViewMatrices {
     world_to_camera: Matrix4<f32>,
     camera_to_ndc: Matrix4<f32>,
 }
 
-#[derive(glace::Uniform)]
-#[glace(bindings = UniformBindings)]
-struct Uniform {
-    view_matrices: ViewMatrices,
-    texture: Texture2d,
-}
-
-#[derive(glace::Vertex)]
-struct Vertex {
+#[derive(Vertex)]
+struct TexVertex {
     position: Vector3<f32>,
     uv: Vector2<f32>,
 }
 
-#[derive(glace::Varying)]
-struct Varying {
+#[derive(ConstInput)]
+struct Consts {
+    radians_per_sec: f32,
+}
+
+#[derive(UniformInput)]
+#[glace(bindings = UniformBindings)]
+struct Uniforms {
+    view_matrices: ViewMatrices,
+    texture: Texture2d,
+}
+
+#[derive(VertexOutput)]
+struct Varyings {
     color: Vector3<f32>,
 }
 
-#[derive(glace::Fragment)]
+#[derive(FragmentOutput)]
 struct Fragment {
     albedo: Vector3<f32>,
     normal: Vector3<f32>,
@@ -38,25 +38,23 @@ struct Fragment {
 
 #[glace(vertex)]
 fn vertex(
-    define: &Define,
-    uniform: &Uniform,
+    uniforms: &Uniforms,
     vertex: &Vertex,
-    varying: &mut Varying,
+    varyings: &mut Varyings,
 ) {
-    vec3 color = texture(uniform.texture, vertex.uv);
-    varying.position = uniform.view_matrices.camera_to_ndc
-        * uniform.view_matrices.world_to_camera
+    varyings.position = uniforms.view_matrices.camera_to_ndc
+        * uniforms.view_matrices.world_to_camera
         * vec4(vertex.position, 1.0);
+    varyings.color = texture(uniforms.texture, vertex.uv);
 }
 
 #[glace(fragment)]
 fn fragment(
-    define: &Define,
-    uniform: &Uniform,
-    varying: &Varying,
+    uniforms: &Uniforms,
+    varyings: &Varyings,
     fragment: &mut Fragment,
 ) {
-    fragment.albedo = varying.color;
+    fragment.albedo = varyings.color;
     fragment.normal = vec3(1.0, 0.0, 0.0);
 }
 
@@ -73,7 +71,7 @@ impl Renderer {
         };
         let program = Program::new(glace, vertex, fragment, define)?;
         let view_matrices = UniformBuffer::new(glace, ViewMatrices::default())?;
-        let texture = Texture::load("bla.png")?;
+        let texture = Texture::new(glace)?;
 
         Ok(Self {
             program,
