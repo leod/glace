0013eae7ce8d35b7110e9063b53008072d08d16b
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-03-02 20:49:45 +0100

    Add program definition structs

--- glace-program/src/sig.rs
+++ glace-program/src/sig.rs
@@ -1,30 +1,30 @@
 use crevice::{glsl::GlslStruct, std140::AsStd140};
 use glsl::syntax::{Expr, TypeSpecifier};
 
-pub trait NamedFields {
-    const FIELD_NAMES: &'static [&'static str];
+#[derive(Debug, Clone, PartialEq)]
+pub struct Field {
+    pub name: &'static str,
+    pub type_specifier: TypeSpecifier,
 }
 
-pub trait ConstInput: NamedFields {
+pub trait Fields {
+    const FIELDS: &'static [Field];
+}
+
+pub trait ConstInput: Fields {
     fn const_values(&self) -> Vec<Expr>;
 }
 
 pub trait Uniform: AsStd140 + GlslStruct {}
 
-pub trait UniformInput: NamedFields {}
+pub trait UniformInput: Fields {}
 
-#[derive(Debug, Clone)]
-pub struct Attribute {
-    pub offset: usize,
-    pub type_spec: TypeSpecifier,
+pub trait Vertex: Fields {
+    const OFFSETS: &'static [usize];
 }
 
-pub trait Vertex: NamedFields {
-    const ATTRIBUTES: &'static [Attribute];
-}
+pub trait VertexInput: Fields {}
 
-pub trait VertexInput: NamedFields {}
+pub trait VertexOutput: Fields {}
 
-pub trait VertexOutput: NamedFields {}
-
-pub trait FragmentOutput: NamedFields {}
+pub trait FragmentOutput: Fields {}
