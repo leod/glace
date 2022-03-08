63a63e411763828d8425b59a01f5924c3b710a22
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-03-08 11:52:43 +0100

    Fix uniform name

--- glace-program/examples/input.rs
+++ glace-program/examples/input.rs
@@ -1,5 +1,8 @@
 use crevice::{glsl::GlslStruct, std140::AsStd140};
-use glsl::transpiler::glsl::show_block;
+use glsl::{
+    syntax::Declaration,
+    transpiler::glsl::{show_block, show_declaration},
+};
 
 #[derive(GlslStruct, AsStd140)]
 struct CameraMatrices {
@@ -12,6 +15,9 @@ fn main() {
 
     let mut s = String::new();
     show_block(&mut s, &tree);
+    println!("{}", s);
 
+    let mut s = String::new();
+    show_declaration(&mut s, &Declaration::Block(tree));
     println!("{}", s);
 }
